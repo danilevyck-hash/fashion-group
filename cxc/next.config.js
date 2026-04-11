7c9efb7d757c1f74d3cafa13b3f6b8de158572ef
danilevyck-hash <danilevyck@gmail.com>
2026-04-10 19:23:34 -0500

    feat: add Sentry error monitoring

--- cxc/next.config.js
+++ cxc/next.config.js
@@ -1,3 +1,5 @@
+const { withSentryConfig } = require("@sentry/nextjs");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -12,4 +14,11 @@ const nextConfig = {
   compress: true,
   poweredByHeader: false,
 };
-module.exports = nextConfig;
+
+module.exports = withSentryConfig(nextConfig, {
+  silent: true,
+  org: "fashion-group",
+  project: "fashion-group",
+  widenClientFileUpload: true,
+  disableLogger: true,
+});
